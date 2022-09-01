3738543cde2e0a3061f8ee3c7764c99c796a6764
USER <USER@DESKTOP-0IE7C7E>
2022-09-01 17:13:29 +0530

    Commit

--- qspautomation/src/qsp/demo1.java
+++ qspautomation/src/qsp/demo1.java
@@ -1,9 +1,9 @@
 package qsp;
 
 public class demo1 {
-  public staic void main(String[]args)
+  public static void main(String[]args)
   {
-    System.out.println("Hello world")
+    System.out.println("Hello world");
   }
 
 }
